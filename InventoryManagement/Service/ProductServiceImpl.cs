d5797ab105c56a0dc766dc3a0f75a14144aecf11
Nadiya-badusha <nadiyabadusha129@gmail.com>
2025-06-09 09:43:52 +0530

    Add files via upload

---
+++ InventoryManagement/Service/ProductServiceImpl.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using InventoryManagement.Model;
+using InventoryManagement.Repository;
+
+namespace InventoryManagement.Service
+{
+    public class ProductServiceImpl : IProductService
+    {
+        private readonly IProductRepository _productRepository;
+
+        public ProductServiceImpl(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        public async Task AddProductAsync(Product product)
+        {
+            await _productRepository.AddProductAsync(product);
+        }
+
+        public async Task UpdateProductAsync(Product product)
+        {
+            await _productRepository.UpdateProductAsync(product);
+        }
+
+        public async Task DeleteProductAsync(string productCode)
+        {
+            await _productRepository.DeleteProductAsync(productCode);
+        }
+
+        public async Task<List<Product>> GetAllProductsAsync()
+        {
+            return await _productRepository.GetAllProductsAsync();
+        }
+
+        public async Task<Product> GetProductByCodeAsync(string productCode)
+        {
+            return await _productRepository.GetProductByCodeAsync(productCode);
+        }
+    }
+}
+
